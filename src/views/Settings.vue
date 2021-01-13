7b402dfa4ae0a41d7b1bdbba0d3e1cb5c5a45313
Flexusma <luregh@gmail.com>
2021-01-13 11:58:15 +0100

    .

---
+++ src/views/Settings.vue
@@ -0,0 +1,15 @@
+<template>
+  <Hero title="Settings" />
+</template>
+
+<script>
+import Hero from "@/components/partials/Hero";
+export default {
+name: "Settings",
+  components: {Hero}
+}
+</script>
+
+<style scoped>
+
+</style>
